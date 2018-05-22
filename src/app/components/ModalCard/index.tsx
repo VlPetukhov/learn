8da7ef5bc2f51bd624f69b4e8c4e1aa5b0632bd3
Vladimir Petukhov <vladimir.petukhov@fastdev.se>
2018-05-22 18:17:47 +0500

    Added ModalCard component

---
+++ src/app/components/ModalCard/index.tsx
@@ -0,0 +1,43 @@
+import * as React from "react";
+
+export interface IModalCardProps {
+    visible: boolean;
+    onClose: () => void;
+    children?: React.ReactNode;
+    buttons?: IButtonData[];
+}
+
+export interface IButtonData {
+    text: string;
+    onClick: () => void;
+    className?: string;
+    disabled?: boolean;
+    hint?: string;
+}
+
+export const ModalCard = (props: IModalCardProps) => (
+    <div className={"modal" + (props.visible ? " is-active" : "")}>
+        <div className="modal-background" />
+        <div className="modal-card">
+            <header className="modal-card-head">
+                <p className="modal-card-title">Modal title</p>
+                <button className="delete" aria-label="close" onClick={props.onClose}/>
+            </header>
+            <section className="modal-card-body">
+                {props.children}
+            </section>
+            {props.buttons && props.buttons.length &&
+                <footer className="modal-card-foot">
+                    { props.buttons.map((btnData: IButtonData, idx: number) => (
+                        <button className={"button " + btnData.className || ""}
+                            title={btnData.hint || ""}
+                            disabled={btnData.disabled}
+                            onClick={btnData.onClick}
+                                key={idx}
+                        >{btnData.text}</button>
+                    )) }
+                </footer>
+            }
+        </div>
+    </div>
+);
--- src/app/pages/Home.tsx
+++ src/app/pages/Home.tsx
@@ -1,9 +1,31 @@
 import * as React from "react";
+import { ModalCard } from "app/components/ModalCard";
 
 export interface IHomeProps {
     children?: React.ReactElement<any>;
 }
 
-export const Home = (props: IHomeProps) => (
-    <h2>Home{ console.log('Home') }</h2>
-);
+export class Home extends React.Component<IHomeProps, {visible: boolean;}> {
+    constructor(props: IHomeProps) {
+        super(props);
+
+        this.state = {
+            visible: false,
+        }
+    }
+
+    public render() {
+        return (
+            <>
+            <h2 onClick={() => this.setState((prevState => ({visible: !prevState.visible})))}>Home{ console.log('Home') }</h2>
+            <ModalCard
+                visible={ this.state.visible }
+                onClose={() => this.setState((prevState => ({visible: !prevState.visible})))}
+                buttons={[{text: "Hi", onClick: ()=> console.log("Hi")}, {text: "Hey", onClick: ()=> console.log("Hey")}]}
+            >
+                Hi! I'm ModalCard.
+            </ModalCard>
+            </>
+        );
+    }
+}
